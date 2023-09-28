0e1e6a1cb2491121f2e16193121b04b96c5d247d
na8874 <104676187+na8874@users.noreply.github.com>
2023-09-28 16:53:49 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,redirect,request
+app=Flask(__name__)
+
+@app.route('/',methods=['GET',"POST"])
+def login():
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/clothe')
+def clothe():
+    return render_template('clothe.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/index1',methods=['GET','POST'])
+def index1():
+    if request.method == 'POST':
+        a = request.form['username']
+        b = request.form['Password']
+
+        print(a,b)
+        if(a=="nikhil@gmail.com" and b=="12345"):
+            return render_template('index.html')
+        else:
+            return render_template('login.html',error = 'Enter Correct Login Credentials')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
